1ad2472bcd0f403c47ac0937884854c322017ba0
PRAFULL <PRAFULL@DESKTOP-DS6TEV0>
2022-08-29 23:56:49 +0530

    [Praful]: adding new file demo3

---
+++ src/Sample1/demo3.java
@@ -0,0 +1,12 @@
+package Sample1;
+
+public class demo3 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi..Praful here");
+		System.out.println("Have a good day");
+		
+		
+	}
+}
